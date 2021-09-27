559efe285f4008eb777028723f5c9ded9e1286df
myruldeen <funstudy10@gmail.com>
2021-09-27 16:53:50 +0800

    add persistent jsonfile
    improve request by adding persistent file

--- routes/post.js
+++ routes/post.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const axios = require('axios');
+const jsonfile = require('jsonfile');
 const router = express.Router();
 
-async function getAllPost(cb) {
-    try {
-        let v = await axios.get('https://jsonplaceholder.typicode.com/posts');
-        cb(v.data)
-    } catch (error) {
-        cb(error.message)
-    }
+
+async function retAndSave(){
+    const data = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    let promises = data.data.map((val) => {
+        return getTotalComment(val.id).then((result) => {
+            return {
+                post_id: val.id,
+                post_title: val.title,
+                post_body: val.body,
+                total_number_of_comments: result.length
+            }
+        }).catch((err) => console.log(err.message));
+    })
+    Promise.all(promises).then((data) => {
+        jsonfile.writeFile('./frontend-test/data.json', data, { spaces: 2 }).then(() => {
+            console.log('file is write');
+        }).catch((err) => console.log(err));
+    }).catch((err) => console.log(err.message));
 }
+retAndSave();
 
 function paginate(array, limit, page_num) {
     return array.slice((page_num - 1) * limit, page_num * limit);
@@ -23,47 +36,19 @@ router.get('/', async (req, res) => {
         limit = 100;
         page = 1;
     }
-    getAllPost((data, err) => {
-        if (err) throw err;
-        let promises = data.map((val) => {
-            return getTotalComment(val.id).then((result) => {
-                return {
-                    post_id: val.id,
-                    post_title: val.title,
-                    post_body: val.body,
-                    total_number_of_comments: result.length
-                }
-            }).catch((err) => console.log(err.message));
-        })
-        Promise.all(promises).then((data) => {
-            let sortedArray = data.sort(function (a, b) { return b.total_number_of_comments - a.total_number_of_comments; });
-            res.json(paginate(sortedArray, limit, page));
-        }).catch((err) => console.log(err.message));
-    })
-
-
+    jsonfile.readFile('./frontend-test/data.json').then((data) => {
+        let sortedArray = data.sort(function (a, b) { return b.total_number_of_comments - a.total_number_of_comments; });
+        res.json(paginate(sortedArray, limit, page));
+    }).catch((err) => console.log(err));
 })
 
 router.get('/:id', (req, res) => {
-    getAllPost((data, err) => {
-        if (err) throw err;
-        let promises = data.map((val) => {
-            return getTotalComment(val.id).then((result) => {
-                return {
-                    post_id: val.id,
-                    post_title: val.title,
-                    post_body: val.body,
-                    total_number_of_comments: result.length
-                }
-            }).catch((err) => console.log(err.message));
-        })
-        Promise.all(promises).then((data) => {
-            const d = data.filter((r) => r.post_id === +req.params.id);
-            res.json(d)
-        }).catch((err) => console.log(err.message));
-    })
-})
 
+    jsonfile.readFile('./frontend-test/data.json').then((data) => {
+        const d = data.filter((r) => r.post_id === +req.params.id);
+        res.json(d)
+    }).catch((err) => console.log(err));
+})
 
 function getTotalComment(id) {
     const promise = new Promise((resolve, reject) => {
